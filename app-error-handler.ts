d0468ffe9b2254e11d5cacb84add7610f763a99c
arun6777 <41993526+arun6777@users.noreply.github.com>
2018-08-01 16:32:48 +0530

    Delete app-error-handler.ts

--- app-error-handler.ts
+++
@@ -1,9 +0,0 @@
-import { ErrorHandler } from "@angular/core";
-
-export class AppErrorHandler implements ErrorHandler{
-    handleError(error){
-        alert('Unexpected apperrorhandler error occured');
-        console.log(error);
-
-    }
-}
